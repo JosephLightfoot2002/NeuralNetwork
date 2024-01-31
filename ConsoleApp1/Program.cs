43373965a2c6e5f67b5feecd640c21e08a18cdfd
JosephLightfoot2002 <120800666+JosephLightfoot2002@users.noreply.github.com>
2024-01-31 17:18:29 +0000

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+using MatrixSpace;
+using NeuralNetworkSpace;
+
+// double[][] trainingInput = [[0,0],[1,0],[0,1],[1,1]];
+// Random rnd= new Random();
+
+
+// double[][] trainingOutput = [[0],[0],[0],[1]];
+
+var hi=CsvReader.Read();
+
+int[,] dims = {{10,hi[0][0].Length}};
+NeuralNetwork  neural= new NeuralNetwork(dims);
+for(int i=0;i<10000;i++){
+    var sample= RandomMethods.StochasticChoice(hi,150);
+    double[][] trainingInput=sample[0];
+    double[][] trainingOutput=sample[1];
+    neural.TrainNetwork(trainingInput,trainingOutput);
+    if(i%10==0){
+        Console.WriteLine(i);
+    }
+}
+new Matrix(hi[1][0]).PrintMatrix();
+neural.Run(hi[0][0]).PrintMatrix();
+// var hi=neural.Run(trainingInput[0]);
+// hi.PrintMatrix();
+// hi=neural.Run(trainingInput[1]);
+// hi.PrintMatrix();
+// hi=neural.Run(trainingInput[2]);
+// hi.PrintMatrix();
+// hi=neural.Run(trainingInput[3]);
+// hi.PrintMatrix();
+
